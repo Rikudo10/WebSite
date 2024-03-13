0b925dd20a9a13538d61d4b502593b5998be2e23
Rikudo10 <giorgisamxaradze16@gmail.com>
2024-03-13 13:30:49 +0400

    Donate

---
+++ src/views/pages/Donate.vue
@@ -0,0 +1,12 @@
+<script setup>
+import mainLayout from '../../layouts/mainLayout.vue';
+import Donate from '../../components/DonateComponents/Donate.vue';
+import Donation from '../../components/DonateComponents/Donation.vue';
+</script>
+
+<template>
+ <mainLayout>
+  <Donate />
+  <Donation />
+  </mainLayout>
+</template>
