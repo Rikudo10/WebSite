d678aaa5300bab0bc39fdd6cbc1f5a36884e2c46
Rikudo10 <giorgisamxaradze16@gmail.com>
2024-03-13 04:53:50 +0400

    home page

---
+++ src/components/HomeComponents/HeroSection.vue
@@ -0,0 +1,31 @@
+<script setup>
+import useHome from "../../composable/useHome";
+
+const { HomePage } = useHome();
+</script>
+<template>
+  <div class="relative">
+    <div class="relative">
+      <img
+        class="w-full object-cover h-[700px] absolute"
+        src="../../assets/Images/Cover.jpg"
+        alt=""
+      />
+    </div>
+    <div class="z-10 relative w-1/2 p-4 mx-auto">
+      <div class="mt-56">
+        <div class="w-[450px]">
+          <h1 class="text-white text-6xl pb-6 font-bold" v-text="HomePage[0].hero.title" />
+        </div>
+        <div class="w-[390px]">
+          <p class="text-white text-lg pb-6" v-text="HomePage[0].hero.description" />
+        </div>
+        <button
+          class="bg-transparent hover:bg-[white] text-[white] font-semibold hover:text-black py-2 px-6 border border-white rounded"
+        >
+          Shop Merch
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
--- src/composable/useHome.js
+++ src/composable/useHome.js
@@ -3,10 +3,29 @@ import { ref } from "vue";
 export default (function useHome() {
     const HomePage = [
         {
-          Cards: {
-            title: "Your Title",
-            // other properties...
+          hero: {
+            title: "Serving you since 1989. ",
+            description: "Acme Outdoors is an outdoor and adventure shop located in the Boathouse District in Oklahoma City.   ",
+            title2: "How we're keeping you safe during COVID-19",
+            description2: "As an outdoor shop, we’ve taken precautionary measures to ensure the safety of all our customers and team members.",
           },
+          cards: [{
+            number: "01",
+            title: "SHOP PRODUCTS",
+            description: "Our full product line is still available online here on our site! Getting outside and hiking is still something you can do. Get your gear now!",
+          },
+        {
+            number: "02",
+            title: "DONATE",
+            description: "Since we've changed the way we operate to online only, and to ensure your safety, not all our staff is working. Donate to keep them afloat.",
+        },
+        {
+          number: "03",
+          title: "BUY GIFT CARDS",
+          description: "Have all the outdoor gear you need for now? Buy a gift card and use it later or share it with friends and family.",
+      },
+        ],
+
         },
         // more objects...
       ];;
