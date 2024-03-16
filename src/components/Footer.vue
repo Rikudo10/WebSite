d0147bd2965226504a619fea5203a6eb7d835ad9
Rikudo10 <giorgisamxaradze16@gmail.com>
2024-03-17 01:23:40 +0400

    finish

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,10 +3,12 @@ import Logo from "../assets/icons/logo.vue";
 </script>
 <template>
   <footer class="bg-[#222222] pb-4 p-16">
-    <div class="w-full max-w-screen-xl mx-auto flex flex-col gap-y-14">
+    <div class="w-full  mx-auto flex flex-col gap-y-14">
       <div class="flex items-center justify-between px-5">
+        <div class="ml-[17%]">
         <Logo />
-        <div class="flex flex-wrap items-center gap-4">
+      </div>
+        <div class="flex flex-wrap items-center gap-4 mr-[17%]">
           <img class="w-6 h-6" src="../assets/icons/Twitter.svg" alt="Logo" />
           <img class="w-6 h-6" src="../assets/icons/Facebook.svg" alt="Logo" />
           <img class="w-6 h-6" src="../assets/icons/Insta.svg" alt="Logo" />
