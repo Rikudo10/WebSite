b9331ae5c53e1addfa536248daec50c29c813476
Levan-Pkhaladze <levani.pkhaladze20@gmail.com>
2024-03-22 01:08:25 +0400

    l

--- src/views/pages/About.vue
+++ src/views/pages/About.vue
@@ -1,7 +1,7 @@
 <script setup>
 import mainLayout from "../../layouts/mainLayout.vue";
 import AboutAdventure from "../../components/AboutComponents/AboutAdventure.vue";
-import BusinessComponent from "../../components/AboutComponents/businessComponent.vue"
+import BusinessComponent from "../../components/AboutComponents/BusinessComponent.vue";
 import Owners from "../../components/AboutComponents/Owners.vue";
 import Owners2 from "../../components/AboutComponents/Owners2.vue";
 import Help from "../../components/AboutComponents/Help.vue";
