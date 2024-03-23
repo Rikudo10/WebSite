92c5e9c633fc3d9615a9d2d8aaed0ca62939c274
Rikudo10 <giorgisamxaradze16@gmail.com>
2024-03-24 01:36:48 +0400

    responsive

--- src/components/HomeComponents/HeroSection.vue
+++ src/components/HomeComponents/HeroSection.vue
@@ -7,17 +7,17 @@ const { HomePage } = useHome();
   <div class="relative">
     <div class="relative">
       <img
-        class="w-full object-cover h-[700px] absolute"
+        class="w-full object-cover h-[550px] xl:h-[700px] absolute"
         src="../../assets/Images/Cover.jpg"
         alt=""
       />
     </div>
-    <div class="z-10 relative w-[62%] p-4 mx-auto">
+    <div class="z-10 relative w-[62%] p-4 xl:mx-auto">
       <div class="mt-56">
-        <div class="w-[450px]">
-          <h1 class="text-white text-6xl pb-6 font-bold" v-text="HomePage[0].hero.title" />
+        <div class="xl:w-[450px]">
+          <h1 class="text-white xl:text-6xl text-lg pb-6 font-bold" v-text="HomePage[0].hero.title" />
         </div>
-        <div class="w-[390px]">
+        <div class="xl:w-[390px]">
           <p class="text-white text-lg pb-6" v-text="HomePage[0].hero.description" />
         </div>
         <button
